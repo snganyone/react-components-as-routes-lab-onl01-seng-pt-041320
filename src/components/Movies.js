a5a20efb9729ecf96a7fa797fcf7302539f9cd39
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2021-01-10 22:05:59 -0800

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,16 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      <ul>
+      {movies.map((r, i) => (
+        <div className="actor">
+        <h3>{r.title}</h3>
+        <h3>{r.time}</h3>
+        <li key={i}>Genres: {r.genres.join(", ")}</li>
+        </div>
+      ))}
+      </ul>
     </div>
   );
 };
